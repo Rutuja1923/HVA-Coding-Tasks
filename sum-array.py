78efacbc85e195c2b90b6b086ee246597b681e1a
Rutuja1923 <rutujajirage19@gmail.com>
2024-10-07 09:07:09 +0530

    Sum of array elements

---
+++ sum-array.py
@@ -0,0 +1,15 @@
+def find_len(lst):
+    count=0
+    while lst:
+        count+=1
+        lst=lst[1:]
+    return count
+print("Enter array elements :")
+L=list(map(int,input().split()))
+l = find_len(L)
+i=0
+sum=0
+while i<l:
+    sum+=L[i]
+    i+=1
+print(sum)
