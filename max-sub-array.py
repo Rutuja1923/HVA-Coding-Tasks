f637909c2aa9a0fadb571235462c8ab5d552bbb0
Rutuja1923 <rutujajirage19@gmail.com>
2025-01-15 15:42:14 +0530

    Iterative approach

--- max-sub-array.py
+++ max-sub-array.py
@@ -1,32 +1,20 @@
-def find_len(lst):
-    count=0
-    while lst:
-        count+=1
-        lst=lst[1:]
-    return count
-
-#using recursion
-
-def longest_increasing_subarray(arr, n, prev_val=None, length=1, max_length=1):
-    if n == 0:                          #base case
-        return max_length
+def longist_increasing_subarray(arr) :
+    if not arr :
+        return 0
     
-    if prev_val is None:                #previous value - first element of subarray
-        prev_val = arr[n-1]
+    curr_len = 1
+    max_len = 1
 
-    if arr[n-1] > prev_val:             #check if previous element is smaller than current element
-        length += 1
-        if length > max_length:
-            max_length = length         # update value of max-length
-    else:                               # Reset length since it's not increasing
-        length = 1  
+    for i in range(1, len(arr)) :
+        if arr[i] > arr[i-1] :
+            curr_len += 1
+            max_len = max(max_len, curr_len)
+        
+        else :
+            curr_len = 1
+        
+    return max_len
 
-    prev_val = arr[n-1]
-    
-    return longest_increasing_subarray(arr, n-1, prev_val, length, max_length)
+L = list(map(int, input().split()))
 
-print("Enter array elements :")
-L=list(map(int,input().split()))
-n = find_len(L)
-longest_length = longest_increasing_subarray(L, n)
-print(longest_length)
+print(longist_increasing_subarray(L))
